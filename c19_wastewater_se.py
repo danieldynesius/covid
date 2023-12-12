e117aec4d28583d056027a3c97e744699791c318
Daniel Dynesius <daniel.dynesius@stratega.ai>
2023-12-12 01:30:57 +0100

    bugfix: year-week x-axis created incorrect y-values. Resolved bug by switching to date for start-of-week on x-axis

--- c19_wastewater_se.py
+++ c19_wastewater_se.py
@@ -1,8 +1,8 @@
 import pandas as pd
-import datetime
 import plotly.graph_objects as go
 import plotly.express as px
 from datetime import datetime as dt
+from datetime import timedelta
 from plotly.io import write_image
 import math
 import pandas as pd
@@ -10,13 +10,15 @@ from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import numpy as np
 
-# Fill all missing weeks attempt. Set to false because not working as intended
-fill_missing_weeks = False
+# Fill all missing weeks attempt. Set to false because True DOES NOT DRAW DATAPOINTS CORRECTLY!
+fill_missing_weeks = True
 
 wastewater_data = pd.read_csv(
     "https://blobserver.dc.scilifelab.se/blob/SLU_wastewater_data.csv",
     sep=",",
 )
+#wastewater_data.to_csv('./SLU_wastewater_data.csv', index=False)
+
 wastewater_data["year"] = (wastewater_data["week"].str[:4]).astype(int)
 wastewater_data["week_no"] = wastewater_data["week"].str.replace(r"\*$", "", regex=True)
 wastewater_data["week"] = wastewater_data["week"].str.replace(r"\*$", "", regex=True)
@@ -33,7 +35,8 @@ def get_first_day(row):
     return dt.fromisocalendar(row['iso_year'], row['iso_week'], 1)
 
 # Apply the function to create a new column "first_day"
-wastewater_data['first_day'] = wastewater_data.apply(get_first_day, axis=1)
+wastewater_data['date'] = wastewater_data.apply(get_first_day, axis=1)
+#wastewater_data = wastewater_data[wastewater_data['channel']=='Östhammar']
 
 # The below accomodates a change in the column title for the COVID data
 wastewater_data.rename(
@@ -42,6 +45,15 @@ wastewater_data.rename(
     },
     inplace=True,
 )
+
+""" grouped = wastewater_data[['channel', 'date']].groupby('channel')
+grouped = grouped.max().reset_index()
+fresh_data_cutoff = dt.today() - timedelta(days=365)
+grouped = grouped[grouped['date'] > fresh_data_cutoff]
+
+wastewater_data = wastewater_data[wastewater_data.channel.isin(grouped.channel)]
+ """
+
 
 if fill_missing_weeks == True:
     ## DD ADD ALL WEEKS
@@ -63,7 +75,7 @@ if fill_missing_weeks == True:
     # Add a flag column indicating whether the datapoint is added (1) or existed already (0)
     result_df['synthethic_datapoint_flg'] = result_df['week'].isna().astype(int)
     #result_df[result_df['synthethic_datapoint_flg']==1].relative_copy_number = -1
-    result_df.loc[result_df.synthethic_datapoint_flg == 1, ['relative_copy_number']] = -1
+    result_df.loc[result_df.synthethic_datapoint_flg == 1, ['relative_copy_number']] = np.nan #-1 #np.nan
 
     # Fill NaN values in other columns if needed
     #result_df = result_df.fillna({'other_column': default_value})
@@ -73,6 +85,8 @@ if fill_missing_weeks == True:
     # If you want to sort the resulting DataFrame by 'channel' and 'date', you can do:
     result_df = result_df.sort_values(by=['channel', 'date']).reset_index(drop=True)
     #df = result_df[['channel','week', 'relative_copy_number']]
+
+    result_df = result_df.drop_duplicates()
     df = result_df
     # Now, result_df contains the DataFrame with missing dates filled for each channel
     # The 'flag' column indicates whether the datapoint is added or existed already
@@ -81,12 +95,14 @@ else:
     df = wastewater_data
 
 
-df.dropna(subset=['channel'], inplace=True)
-unique_areas = list(df.channel.unique())
+df.dropna(subset=['channel'], inplace=True) # drop nan in channel value
+unique_areas = list(df.channel.sort_values().unique())
 
 n_unique_areas = len(unique_areas)
 n_cols_for_output = 4 # user specified
 n_rows_for_output = math.ceil(n_unique_areas/n_cols_for_output) # needed based on n areas in data
+
+df = df[['week', 'channel', 'relative_copy_number', 'iso_week', 'date']]
 
 fig = make_subplots(
     rows=n_rows_for_output, cols=n_cols_for_output,
@@ -101,12 +117,13 @@ for i, area in enumerate(unique_areas, start=1):
     row_num = (i - 1) // n_cols_for_output + 1
     col_num = (i - 1) % n_cols_for_output + 1
     print('Current area:', area, 'RC:',row_num, col_num)
+    area_x_series = df[df['channel'].str.lower() == area_lowercase].date
+    area_y_series = df[df['channel'].str.lower() == area_lowercase].relative_copy_number
 
-    #test_list = list(df[df['channel'].str.lower() == area_lowercase].relative_copy_number)
     fig.add_trace(
         go.Scatter(
-            x=df[df['channel'].str.lower() == area_lowercase].week
-            ,y=df[df['channel'].str.lower() == area_lowercase].relative_copy_number
+            x=area_x_series
+            ,y=area_y_series
             #,y=test_list
             ,mode='lines+markers'
             ,connectgaps=True
@@ -115,16 +132,11 @@ for i, area in enumerate(unique_areas, start=1):
         row=row_num, col=col_num
     )
 
-#df['week'] = pd.to_datetime(df['week'] + '-1', format='%Y-%U-%w')
-#ticktext = df['week'].dt.strftime('%Y-%U')  # Format as year-weeknumber
-
-#fig.update_xaxes(ticktext=ticktext)
-#fig.update_traces(connectgaps=False)
-
-
 fig.update_layout(
-    height=50*n_unique_areas, width=300*n_cols_for_output,  # Adjust width to accommodate all subplots
+    height=300 + (50*n_unique_areas), width=300*n_cols_for_output,  # Adjust width to accommodate all subplots
     title_text="Covid-19 Wastewater Sweden"
 )
+fig.update_xaxes(tickangle=45, tickfont=dict(family='Rockwell', color='black', size=14))
+#fig.update_yaxes(rangemode="tozero")
 
-fig.show()                  
+fig.show()
