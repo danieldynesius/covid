6367a728fa569c5c2506af959f3e531e7ac8f43c
Daniel Dynesius <daniel.dynesius@stratega.ai>
2023-12-18 16:35:41 +0100

    normalized folium geomap

--- dataprocessing/3_read_staged_write_to_final/read_all_staged_create_geomap.py
+++ dataprocessing/3_read_staged_write_to_final/read_all_staged_create_geomap.py
@@ -1,45 +1,35 @@
 import pandas as pd
-import pyarrow
 import geopandas as gpd
 from branca.colormap import linear
 import folium
 from folium import plugins
-import os
 
-
-
-
-# Read the Parquet file into a new Pandas DataFrame
-
+# Read the Parquet files into GeoDataFrames
 g1 = gpd.read_parquet('~/code/analytics/covid/data/2_staged_data/france_wastewater.parquet')
 g2 = gpd.read_parquet('~/code/analytics/covid/data/2_staged_data/sweden_wastewater.parquet')
-list_gdf = [g1,g2]
 
-gdf = gpd.GeoDataFrame( pd.concat( [g1, g2], ignore_index=True) )
+# Concatenate GeoDataFrames
+gdf = gpd.GeoDataFrame(pd.concat([g1, g2], ignore_index=True))
 
+# Create a color scale for each cntr_code
+color_scales = {}
+for cntr_code, cntr_code_df in gdf.groupby('cntr_code'):
+    color_scales[cntr_code] = linear.OrRd_04.scale(min(cntr_code_df['value']), max(cntr_code_df['value']))
 
-#### Visualize Data
-# Create a color scale - Dynamic
-color_scale = linear.OrRd_04.scale(min(gdf['value']), max(gdf['value']))
-
-# Create a color scale - Static
-#color_scale = linear.OrRd_04.scale(0, 25)
-#color_scale = 'YlOrRd'
-
-# Create a GeoJSON object and features
+# Visualize Data
 geojson_features = []
-
 for _, row in gdf.iterrows():
+    cntr_code = row['cntr_code']
     feature = {
         'type': 'Feature',
         'geometry': row['geometry'].__geo_interface__,
         'properties': {
             'style': {
-                'color': 'black',  # Set the color to black for borders
-                'color': color_scale(row['value']),
-                'fillcolor': color_scale(row['value']),
+                'color': 'black',
+                'color': color_scales[cntr_code](row['value']),
+                'fillcolor': color_scales[cntr_code](row['value']),
                 'opacity': 0.7,
-                'weight': 2,  # Adjust the weight for thicker borders
+                'weight': 2,
                 'fillOpacity': 0.5,
             },
             'time': row['first_day'],
@@ -53,7 +43,7 @@ geojson_data = {
 }
 
 # Create a Folium map centered around France
-m = folium.Map(location=[46.6031, 1.8883], zoom_start=5)
+m = folium.Map(location=[46.6031, 5], zoom_start=3.5)
 
 # Use TimestampedGeoJson to add a timestamp slider
 plugins.TimestampedGeoJson(
@@ -69,9 +59,10 @@ plugins.TimestampedGeoJson(
     date_options='YYYY-MM-DD'
 ).add_to(m)
 
-# Display the color scale in the map
-color_scale.caption = 'Value'
-color_scale.add_to(m)
+# Display color scales for each cntr_code
+#for cntr_code, color_scale in color_scales.items():
+#    color_scale.caption = f'Value - {cntr_code}'
+#    color_scale.add_to(m)
 
 # Display the map
 m
